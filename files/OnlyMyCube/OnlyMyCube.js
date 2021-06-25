a9c384328891b226f3f61ebcd5e7e4d985465364
NingChenhui <1271190462@qq.com>
2021-06-25 16:16:34 +0800

    Site updated: 2021-06-25 16:16:27

--- files/OnlyMyCube/OnlyMyCube.js
+++ files/OnlyMyCube/OnlyMyCube.js
@@ -1,4 +1,4 @@
-var gl;
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>var gl;
 var canvas;
 var program;
 var vPosition;
